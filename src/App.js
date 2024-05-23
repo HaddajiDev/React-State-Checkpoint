9bce47064bb1d549ae3bd9887c20c923d180a6b9
Ahmed Haddaji <137641951+HaddajiDev@users.noreply.github.com>
2024-05-23 16:26:26 +0100

    Check

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {Component, useEffect, useState} from 'react';
+import './App.css';
+
+import Person from './components/Person';
+
+function App(){
+  const person = {
+    full_Name: "Ahmed Haddaji",
+    bio: "some guy",
+    imgSrc: "https://th.bing.com/th/id/OIP.RVS7A90AUbKJPG0scgW7NQHaFz?rs=1&pid=ImgDetMain",
+    job: "Nan"      
+  };
+
+  const [shows, setValue] = useState(true);
+  const [timer, setTimer] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((prevCounter) => prevCounter + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  
+  
+  
+  return(
+      <div className="App">
+        <h1>Hello</h1>
+        <Person person={person} shows={shows}/>
+        <button onClick={()=> setValue(!shows)}>Toggle</button>     
+        <h1>{timer}</h1>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Person.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Person(props) {
+	if(props.shows){
+		return (
+			<div>	
+				<img src={props.person.imgSrc} />
+				<h1>Name : {props.person.full_Name}</h1>
+				<h1>bio : {props.person.bio}</h1>
+				<h1>job : {props.person.job}</h1>
+			</div>
+		);
+	}	
+}
+
+export default Person
