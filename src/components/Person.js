378a0238cb79d23ea40f2f3ad7af714b4a58393d
Ahmed Haddaji <137641951+HaddajiDev@users.noreply.github.com>
2024-05-23 16:30:48 +0100

    check

--- src/components/Person.js
+++ src/components/Person.js
@@ -4,7 +4,7 @@ function Person(props) {
 	if(props.shows){
 		return (
 			<div>	
-				<img src={props.person.imgSrc} />
+				<img src={props.person.imgSrc} alt='img'/>
 				<h1>Name : {props.person.full_Name}</h1>
 				<h1>bio : {props.person.bio}</h1>
 				<h1>job : {props.person.job}</h1>
